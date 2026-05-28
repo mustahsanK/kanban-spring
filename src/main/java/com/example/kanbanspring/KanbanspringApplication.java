9b15edfd57d7fa26246f59902bdec159a40aac35
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2026-05-28 21:20:24 +0530

    Initian Commit

---
+++ src/main/java/com/example/kanbanspring/KanbanspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanspringApplication.class, args);
+	}
+
+}
