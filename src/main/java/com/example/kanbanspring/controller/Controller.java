b387da1dc5ba5e538cfb1f2f1da1a5a97c29042b
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2026-06-18 23:40:33 +0530

    Added simple controller

---
+++ src/main/java/com/example/kanbanspring/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.kanbanspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @GetMapping("")
+  public String kanban() {
+    return "Kanban Board";
+  }
+}
