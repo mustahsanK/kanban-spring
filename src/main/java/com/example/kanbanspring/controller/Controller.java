3b4e95f71f2d3c95e65e2b1d67efe7204f86e73b
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2026-06-20 23:55:07 +0530

    Updated controller

--- src/main/java/com/example/kanbanspring/controller/Controller.java
+++ src/main/java/com/example/kanbanspring/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-  @GetMapping("")
+  @GetMapping("/about")
   public String kanban() {
     return "Kanban Board";
   }
