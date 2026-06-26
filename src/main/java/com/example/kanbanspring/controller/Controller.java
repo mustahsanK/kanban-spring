6acb8782bdffbd0c77a863d189ee0233d55c4bbf
mustahsanK <108030556+mustahsanK@users.noreply.github.com>
2026-06-26 23:53:02 +0530

    Updated controller

--- src/main/java/com/example/kanbanspring/controller/Controller.java
+++ src/main/java/com/example/kanbanspring/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.example.kanbanspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/kanban")
 public class Controller {
 
   @GetMapping("/about")
